3b3ce2c04061e2572faea3ef7ed7267677d317a9
luotianyu2000 <1074885784@qq.com>
2021-05-08 23:10:51 +0800

    36

---
+++ 练习题2.c
@@ -0,0 +1,54 @@
+//字符转数字:(字符数字-字符0=整形数字)
+
+//#include <stdio.h>
+//#include <string.h>
+//void fun(char* p)
+//{
+//	int i;
+//	long int num = 0;
+//	for (i = 0; i < strlen(p); i++)
+//	{
+//		if (*(p + i) >= '0' && *(p + i) <= '9')
+//		{
+//			num = *(p + i) - '0';
+//			printf("%ld", num);
+//		}
+//	}
+//
+//}
+//int main(void)
+//{
+//	char ch[20];
+//	char* p;
+//	printf("请输入一个字符数字:");
+//	p = gets(ch);
+//	printf("输出字符数字:");
+//	puts(p);
+//	//字符转数字
+//	printf("输出整数部分:");
+//	fun(ch);
+//	return 0;
+//
+//}
+
+//#include <stdio.h>
+//#include <string.h>
+//int main(void)
+//{
+//	char ch[20];
+//	char* p;
+//	p = ch;
+//	p = gets(ch);
+//	puts(p);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <string.h>
+//int main(void)
+//{
+//	unsigned short int a;
+//	a = 65535;
+//	printf("%u", a);
+//}
+
